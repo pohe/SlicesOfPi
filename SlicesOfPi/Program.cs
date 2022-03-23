73b5b0f2c7250d8fb502b3fd468e87c30045308c
Poul Henriksen <pohe@easj.dk>
2022-03-23 13:02:49 +0100

    Add project files.

---
+++ SlicesOfPi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SlicesOfPi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PiCalcUI.RunPiCalculation();
+            PiCalcUI.RunPiCalculationAsync();
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
